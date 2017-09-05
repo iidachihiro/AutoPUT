121bea1986acb71cd2fd5d00293b1b98032e99af
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2017-09-05 11:25:11 +0900

    user experiment preparation under implementation

--- src/main/java/jp/mzw/autoput/experiment/Prepare.java
+++ src/main/java/jp/mzw/autoput/experiment/Prepare.java
@@ -1,11 +1,16 @@
 package jp.mzw.autoput.experiment;
 
+import jp.mzw.autoput.core.Project;
+import jp.mzw.autoput.core.TestSuite;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.eclipse.jdt.core.dom.PackageDeclaration;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,15 +18,19 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by TK on 9/1/17.
  */
 public class Prepare {
 
+    public static final String CONFIG_FILENAME = "config.properties";
+
     static final int SIMILAR_METHODS_NUM = 1;
     protected static final String DETECT_RESULT = "detect_result.csv";
     protected static final String CONVERTER_RESULT = "converter_results.csv";
+    protected static final String EXPERIMENTAL_SETTING = "experimental_setting.csv";
     protected static final String[] PROJECTS =
 //            {"commons-codec"};
             {"commons-codec", "commons-collections", "commons-math", "joda-time", "jdom"};
@@ -32,7 +41,15 @@ public class Prepare {
             getTargetsRandomly();
         } else if (command.equals("all")) {
             getAllTargets();
+        } else if (command.equals("prepare")) {
+            prepareExperimentalTargets();
+        } else if (command.equals("test-compile")) {
+            compileCheck();
         }
+    }
+
+    private static String getExperimentalSubjectFile(String project) {
+        return String.join("/", "experiment", "subject", project, EXPERIMENTAL_SETTING);
     }
 
     private static void getTargetsRandomly() {
@@ -57,15 +74,53 @@ public class Prepare {
         for (String project : PROJECTS) {
             System.out.println("Project: " + project);
             List<CSVRecord> convertResults = getConvertResults(project);
-            System.out.println("Size: " + convertResults.size());
-            allSize += convertResults.size();
+            int size = 0;
             for (CSVRecord record : convertResults) {
                 if (record.get(2).equals("1") && record.get(3).equals("1")) {
-                    System.out.println("Class: " + record.get(0) + " , Original: " + record.get(1));
+//                    System.out.println("Class: " + record.get(0) + " , Original: " + record.get(1));
+                    size++;
                 }
             }
+            allSize += size;
+            System.out.println("Size: " + size);
         }
         System.out.println("All Size: " + allSize);
+    }
+
+    private static void prepareExperimentalTargets() throws IOException {
+        for (String projectId : PROJECTS) {
+            Project project = new Project(projectId).setConfig(CONFIG_FILENAME);
+            System.out.println("Project: " + project.getProjectId());
+            List<String> contents = new ArrayList<>();
+            List<CSVRecord> convertResults = getConvertResults(projectId);
+            for (int i = 1; i <= 2; i++) {
+                contents.add("Subject" + i + "\n");
+                int count = 0;
+                while (count < 10) {
+                    if (convertResults.size() <= 0) {
+                        break;
+                    }
+                    Collections.shuffle(convertResults);
+                    CSVRecord record = convertResults.get(0);
+                    convertResults.remove(0);
+                    if (record.get(2).equals("1") && record.get(3).equals("1")) {
+                        String className = record.get(0);
+                        String originName = record.get(1);
+                        contents.add(String.join(",", className, originName));
+                        for (TestSuite testSuite : project.getTestSuites()) {
+                            if (testSuite.getTestClassName().equals(className)) {
+                                PackageDeclaration packageDeclaration = testSuite.getCu().getPackage();
+                                contents.add("," + packageDeclaration.getName());
+                            }
+                        }
+                        contents.add("\n");
+                        count++;
+                    }
+                }
+                contents.add("\n");
+            }
+            _outputExperimentalSubjects(projectId, contents);
+        }
     }
 
 
@@ -93,4 +148,63 @@ public class Prepare {
         return records;
     }
 
+    private static void _outputExperimentalSubjects(String project, List<String> contents) {
+        if (!Files.exists(Paths.get(getExperimentalSubjectFile(project)))) {
+            try {
+                Files.createDirectories(Paths.get(String.join("/", "experiment", "subject", project)));
+                Files.createFile(Paths.get(getExperimentalSubjectFile(project)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Subjects are already decided!");
+            return;
+        }
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(getExperimentalSubjectFile(project)), StandardCharsets.UTF_8)) {
+            for (String content : contents) {
+                bw.write(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void compileCheck() {
+        for (String project : PROJECTS) {
+            System.out.println("Project: " + project);
+            List<CSVRecord> records = getExperimentalSubjects(project);
+            for (CSVRecord record : records) {
+                String className = record.get(0);
+                String originName = record.get(1);
+                String packageName = record.get(2);
+                String content = getConvertedTest(project, className, originName);
+                createFile(packageName, content);
+                compileFile()
+                
+            }
+        }
+
+    }
+
+    private static List<CSVRecord> getExperimentalSubjects(String project) {
+        List<CSVRecord> records = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(getExperimentalSubjectFile(project)), StandardCharsets.UTF_8)) {
+            CSVParser parser = CSVFormat.DEFAULT.parse(br);
+            records = parser.getRecords();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return records;
+    }
+
+    private static String getConvertedTest(String project, String className, String originName) {
+        String content = "";
+        try {
+            content =  Files.lines(Paths.get(String.join("/", "output", project, "convert", className, originName + ".txt"))
+                    , Charset.forName("UTF-8")).collect(Collectors.joining(System.getProperty("line.separator")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
 }
